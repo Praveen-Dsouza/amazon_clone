4fb0c6557a4e6ecc76e6f687f33732d87b944ac3
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-03-15 16:34:00 +0530

    products added

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import '../Styles/Components/Product.scss';
+import StarIcon from '@material-ui/icons/Star';
 
-function Product() {
-  return (
-    <div className='product'>
-        <div className="product_info">
-            <p>The lean startup</p>
-            <p className='product_price'>
-                <small>$</small>
-                <strong>19.99</strong>
-            </p>
-            <div className="product_rating">
-                <p>*</p>
+function Product({ id, title, image, price, rating }) {
+
+    return (
+        <div className='product'>
+            <div className="product_info">
+                <p>{title}</p>
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p><StarIcon className='product_ratinIcon' /></p>
+                    ))}
+                </div>
             </div>
+
+            <img className='product_image' src={image} alt=''/>
+
+            <button className='add_to_cart_button'>Add to Cart</button>
         </div>
-
-        <img className='product_image' src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' alt=''/>
-
-        <button className='add_to_cart_button'>Add to Cart</button>
-    </div>
-  )
+    )
 }
 
 export default Product
